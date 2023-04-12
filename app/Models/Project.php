15996d430342537617ae6c6b691d5e9cb57d6dbc
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-04-12 17:37:17 +0200

    add CRUD & validator

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["name", "programming_languages_used", "start_date", "end_date", "description"];
 }
